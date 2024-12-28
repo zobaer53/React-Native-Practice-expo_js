ebc796ef1314d763c5016b7427552f37cdd12e69
zed <hossain.zobaer11858@gmail.com>
2024-12-28 23:41:53 +0600

    styles --  basic styles practice

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container2}>
+        <Text style={styles.dummyText}>Hello World</Text>
+      </View>
+      <Text style={styles.dummyText}>
+        Open up App.js to start working on your app!
+      </Text>
+
+      <Button style={styles.dummyText} title="Click me" />
     </View>
   );
 }
@@ -13,8 +18,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container2: {
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  dummyText: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
