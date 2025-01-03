938b13ecdbc170ae51061df69adbf78e1c800c0f
zed <hossain.zobaer11858@gmail.com>
2025-01-03 20:57:35 +0600

    lists -- save lists and show

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [enteredGoalText, setEnteredGoalText] = useState(""); // here enteredGoalText is the name of the state value and setEnteredGoalText is the function to update the state value
+  const [goals, setGoals] = useState([]); // here goals is the name of the state value and setGoals is the function to update the state value
 
   function goalInputHandler(enteredText) {
     console.log(enteredText);
@@ -11,6 +12,15 @@ export default function App() {
 
   function addGoalHandler() {
     console.log(enteredGoalText);
+
+    // we can set the goals by calling setGoals() function and passing the new value of goals array as an argument to it.
+    // But we can't directly modify the state value. So, we have to use the previous state value to update the state value.
+    // We can do this by passing a function to the setGoals() function. This function will receive the previous state value as an argument
+    // and we can return the new state value from this function.
+
+    // setGoals([...goals, enteredGoalText]); //this is the wrong way to update the state value. Because we are directly modifying the state value.
+
+    setGoals((currentGoals) => [...currentGoals, enteredGoalText]); //here a function is passed to the setGoals() function. This function will receive the previous state value as an argument and we can return the new state value from this function.
   }
 
   return (
@@ -24,8 +34,16 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
 
+      {/* Here we are displaying the goals in the goalsContainer 
+      
+      */}
+
       <View style={styles.goalsContainer}>
-        <Text> List of goals</Text>
+        {goals.map((goal, index) => (
+          <View key={index} style={styles.goal}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -60,4 +78,5 @@ const styles = StyleSheet.create({
   goalsContainer: {
     flex: 6,
   },
+  goal: { padding: 10, marginVertical: 10, backgroundColor: "#ccc" },
 });
